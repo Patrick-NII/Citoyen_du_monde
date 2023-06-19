a14c9eb69f0e0b50844bf8bed581d4716eb5bdba
Patrick Ngunga <patrick.nii@aol.com>
2023-06-19 13:10:06 +0200

    Revert "Résolution de conflits de fusion avec la branche main"
    This reverts commit 65b0ab4af8a6060b978239189544337aeeb9262c, reversing
    changes made to 93dc13370542008ea8973b3fd27c438c1904af31.

--- assets/script.js
+++ assets/script.js
@@ -46,14 +46,3 @@ arrow.addEventListener('click', function() {
 
 
 
-
-
-  // Récupérer les éléments du DOM
-  const navbarToggle = document.querySelector('.navbar-toggle');
-  const navbarCollapse = document.querySelector('.navbar-collapse');
-
-  // Ajouter un écouteur d'événements au clic sur le bouton de menu
-  navbarToggle.addEventListener('click', () => {
-    // Inverser la visibilité du menu
-    navbarCollapse.classList.toggle('show');
-  });
